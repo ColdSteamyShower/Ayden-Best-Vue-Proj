3bfc60bef9ba1ed9d6af092b384cddd9b0e0e395
Ayden <abest717@gmail.com>
2022-09-03 20:28:05 -0700

    Created mockup for cards

--- pf-challenge/src/App.vue
+++ pf-challenge/src/App.vue
@@ -1,26 +1,65 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="card-container">
+    <CardInstance/>
+    <CardInstance/>
+    <CardInstance/>
+    <CardInstance/>
+    <CardInstance/>
+
+    <CardInstance/>
+    <CardInstance/>
+    <CardInstance/>
+    <CardInstance/>
+    <CardInstance/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CardInstance from './components/CardInstance.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CardInstance
+  },
+  data() {
+    return {
+      dogs: fetch("https://dog.ceo/api/breeds/image/random/10")
+    }
+  },
+  watch: {
+    dogs: function(newData) {
+      console.log("New data for dogs: ", newData)
+    }
   }
 }
 </script>
 
 <style>
+* {
+  margin: 0px;
+  padding: 0px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#card-container {
+  border: 1px dashed red;
+  margin: 0px 10% 0px 10%;
+  height: 70vh;
+  width: 80%;
+  position: absolute;
+  bottom: 5%;
+  left: 0px;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between
 }
 </style>
---
+++ pf-challenge/src/components/CardInstance.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="card-container">
+      <div class="card" :class="{flipped: isClicked}" @click="toggleClicked">
+        <div class="front">
+          <img class="front-icon" src="../assets/bone.svg"/>
+        </div>
+        <div class="back">
+          <header>Header</header>
+          <div id="card-body">Body</div>
+          <footer>Footer</footer>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  
+  export default {
+    name: 'CardInstance',
+    data() {
+      return {isClicked: false}
+    },
+    methods: {
+      toggleClicked: function() {
+        this.isClicked = !this.isClicked;
+      }
+    }
+  }
+  </script>
+  
+  <style>
+    .card-container {
+      height: 40%;
+      margin: 2%;
+      padding: 2px;
+      background-color: transparent;
+      border-radius: 12px;
+      overflow: hidden;
+      flex: 0 1 15%;
+      transition: transform 0.2s ease;
+    }
+
+    .card {
+      position: relative;
+      height: 100%;
+      width: 100%;
+      transform-style: preserve-3d;
+      transition: all 0.6s ease;
+    }
+
+    .card-container:hover {
+      transform: translateY(-10px);
+    }
+
+    .flipped {
+      transform: rotateY(180deg)
+    }
+
+    .front {
+      position: absolute;
+      height: 100%;
+      width: 100%;
+      background-color: rgb(238, 238, 238);
+      backface-visibility: hidden;
+      display: flex;
+      justify-content: center;
+      border: 1px solid lightgray;
+    }
+
+    .back {
+      position: absolute;
+      height: 100%;
+      width: 100%;
+      background-color: gray;
+      transform: rotateY(180deg);
+      backface-visibility: hidden;
+    }
+
+    .front-icon {
+      width: 60%;
+      filter: opacity(10%);
+      transform: rotate(45deg)
+    }
+
+    header, footer {
+      background-color: lightblue;
+      height: 25%;
+      width: 100%;
+    }
+
+    #card-body {
+      height: 50%;
+    }
+  </style>
